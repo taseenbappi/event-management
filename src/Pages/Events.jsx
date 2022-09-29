c4080c46d0923f7d82567531e4067a2d5d2e31c2
taseenbappi <taseen.bappi@gmail.com>
2022-09-30 00:22:18 +0600

    event page added and responsive issues solve

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import cakeImg from "../assets/birthday-cake.png";
+import { BsCalendarWeek } from 'react-icons/bs';
+import { MdOutlineLocationOn, MdKeyboardArrowRight } from 'react-icons/md';
+import "../styles/Events.css";
 
 const Events = () => {
 
     return (
-        <div>
+        <div className="container-fluid event-page">
+            <div className="container">
+                <div className="row ">
 
+                    <div className="col-12 col-sm-12 col-md-12 pt-lg-0 pt-4 pt-md-4 col-lg-6 order-2 order-md-2 order-lg-1">
+
+                        <div className="event-header">
+                            <h1 className='event-title'>Birthday Bash</h1>
+                            <p className='event-author-title'>Hosted by <strong>Elysia</strong></p>
+                        </div>
+
+                        <div className="event-date d-flex  align-items-center  justify-content-between py-3">
+                            <div className="d-flex align-items-center">
+                                <div className="event-icon">
+                                    <BsCalendarWeek className='event-icon-style'></BsCalendarWeek>
+                                </div>
+                                <div className="event-date-details">
+                                    <h1 className='event-title-style'>18 August 6:00PM</h1>
+                                    <p className='event-title-sub-style'>to <strong>19 August 1:00PM</strong> UTC +10</p>
+                                </div>
+                            </div>
+                            <div className="">
+                                <MdKeyboardArrowRight className='fs-4 event-arrow-icon' ></MdKeyboardArrowRight>
+                            </div>
+                        </div>
+
+                        <div className="event-date d-flex  align-items-center  justify-content-between py-3">
+                            <div className="d-flex align-items-center">
+                                <div className="event-icon">
+                                    <MdOutlineLocationOn className='event-icon-style'></MdOutlineLocationOn>
+                                </div>
+                                <div className="event-address-details">
+                                    <h1 className='event-title-style'>Street name</h1>
+                                    <p className='event-title-sub-style'>Suburb, State, Postcode</p>
+                                </div>
+                            </div>
+                            <div className="">
+                                <MdKeyboardArrowRight className='fs-4 event-arrow-icon' ></MdKeyboardArrowRight>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                    <div className="p-0 p-lg-1 p-md-1 col-12 col-sm-12 col-md-12 col-lg-6 order-1 order-md-1 order-lg-2 ">
+                        <img src={cakeImg} alt="" className='img-fluid event-cake-img' />
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "../styles/LandingPage.css";
 import eventImage from "../assets/landing-page-image.svg";
 
 const LandingPage = () => {
 
     return (
-        <div className="container-fluid">
+        <div className="container-fluid landing-page">
             <div className="container">
                 <div className="row align-items-center ">
-                    <div className="col-12 col-sm-12 col-md-12 col-lg-7">
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-6 order-2 order-md-2 order-lg-1">
                         <img src={eventImage} alt="" className='img-fluid landing-event-img' />
                     </div>
-                    <div className="col-12 col-sm-12 col-md-12 col-lg-5">
-                        <h1 className='landing-main-title'>Imagine if <span>Snapchat</span> had events.</h1>
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-6 order-1 order-md-1 order-lg-2 ">
+                        <h1 className='landing-main-title'>Imagine if <br /><span>Snapchat</span><br /> had events.</h1>
                         <p className='landing-sub-title'>Easily host and share events with your friends across any social media.</p>
-                        <button className='create-event-btn d-block ms-auto'>🎉Create my event</button>
+                        <Link to="/create" className='text-decoration-none'>
+                            <button className='create-event-btn d-block ms-auto'>🎉Create my event</button>
+                        </Link>
                     </div>
                 </div>
 
